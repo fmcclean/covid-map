1fd4beadbb933989284063f1a2ba2a5a16b5c6a0
fmcclean <fergus.mcclean@gmail.com>
2020-03-21 15:57:01 +0000

    added boundaries and data

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM continuumio/miniconda
 COPY environment.yml .
 RUN conda env update -n base -f environment.yml
 COPY app.py .
+COPY CountyUAs_cases_table.csv .
+COPY la-boundaries-simple.geojson .
 CMD python app.py -p $PORT
--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import argparse
+import plotly.express as px
+import json
+import pandas as pd
 
 parser = argparse.ArgumentParser()
 
@@ -14,6 +17,23 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+df = pd.read_csv('CountyUAs_cases_table.csv', index_col='GSS_CD')
+
+with open('la-boundaries-simple.geojson') as f:
+    geojson = json.parse(f.read())
+
+fig = px.choropleth_mapbox(df, geojson=geojson, locations='lad19cd', color='TotalCases',
+                           color_continuous_scale="Viridis",
+                           # range_color=(0, 12),
+                           mapbox_style="carto-positron",
+                           zoom=3,
+                           center={"lat": 55, "lon": -1},
+                           opacity=0.5,
+                           labels={'TotalCases': 'Total Cases'}
+                          )
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
+
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
 
@@ -23,15 +43,7 @@ app.layout = html.Div(children=[
 
     dcc.Graph(
         id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
+        figure=fig
     )
 ])
 
