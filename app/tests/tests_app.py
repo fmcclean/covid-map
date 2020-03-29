a9ea6822091f717f19300f6ea13ac685d944b019
fmcclean <fergus.mcclean@gmail.com>
2020-03-29 16:14:03 +0100

    updated entrypoint and test

--- app/tests/tests_app.py
+++ app/tests/tests_app.py
@@ -1,11 +1,16 @@
 import unittest
-from app import app
+import os
+os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 class TestApp(unittest.TestCase):
 
+    def setUp(self):
+        from app import app
+        self.app = app
+
     def test_update_data(self):
-        app.update_data()
+        self.app.update_data()
 
 
 if __name__ == '__main__':
